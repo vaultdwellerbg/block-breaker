898d8c143e0027d8368963a048cc717f4650f870
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-03-12 00:36:58 +0200

    Stick the ball to the paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+
+    Vector2 paddleToBallVector;
+
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
