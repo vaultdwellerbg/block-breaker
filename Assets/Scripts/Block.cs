89d0f9592ad09281ed293d09a9fc606a68ba8523
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-04-01 00:41:40 +0300

    Count breakable blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,14 @@ public class Block : MonoBehaviour
 {
 	[SerializeField] AudioClip destroySound;
 
+	Level level;
+
+	private void Start()
+	{
+		level = FindObjectOfType<Level>();
+		level.CountBreakableBlocks();
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position);
