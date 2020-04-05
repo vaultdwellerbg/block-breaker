9369c6399b35fd38d79e89a2307f3403e9aa3899
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-04-05 20:56:21 +0300

    Add game speed setting in level

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.5f, 2f)] [SerializeField] float gameSpeed = 1f;
+
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
