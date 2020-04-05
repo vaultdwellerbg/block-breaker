261e109d657a41818af73b9d8517b540d6aa8d8f
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-04-05 21:11:28 +0300

    Add game score

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,11 +7,13 @@ public class Block : MonoBehaviour
 	[SerializeField] AudioClip destroySound;
 
 	Level level;
+	GameStatus gameStatus;
 
 	private void Start()
 	{
 		level = FindObjectOfType<Level>();
 		level.CountBreakableBlocks();
+		gameStatus = FindObjectOfType<GameStatus>();
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
@@ -22,6 +24,7 @@ public class Block : MonoBehaviour
 	private void DestroyBlock()
 	{
 		level.BlockDestroyed();
+		gameStatus.AddToScore();
 		AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position);
 		Destroy(gameObject);
 	}
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class GameStatus : MonoBehaviour
 {
     [Range(0.5f, 2f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlock = 5;
+
+    [SerializeField] int currentScore = 0;
 
     void Update()
     {
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore() 
+    {
+        currentScore += pointsPerBlock;
     }
 }
