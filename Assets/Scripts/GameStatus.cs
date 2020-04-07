5428ee19321c4bd0bfc1facf6d86dd75e24c9957
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-04-08 01:05:12 +0300

    Persist score through levels

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class GameStatus : MonoBehaviour
 {
     [Range(0.5f, 2f)] [SerializeField] float gameSpeed = 1f;
@@ -9,6 +10,20 @@ public class GameStatus : MonoBehaviour
     [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     private void Start()
     {
