7ebdc7f75263eeef5cf1ef1fd2a5b2116c68bb72
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-04-15 23:31:50 +0300

    Add ball speedup

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -8,8 +8,13 @@ public class GameSession : MonoBehaviour
     [Range(0.5f, 2f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlock = 5;
     [SerializeField] TextMeshProUGUI scoreText;
-
     [SerializeField] int currentScore = 0;
+
+    [Header("Speedup Settings")]
+    [SerializeField] int pointsToRaiseSpeed = 25;
+    [Range(0.0f, 1f)] [SerializeField] float speedRaise = 0.1f;
+
+    float initialGameSpeed;
 
     private void Awake()
     {
@@ -28,17 +33,38 @@ public class GameSession : MonoBehaviour
     private void Start()
     {
         scoreText.text =  currentScore.ToString();
+        initialGameSpeed = gameSpeed;
     }
 
-    void Update()
+    private void Update()
     {
         Time.timeScale = gameSpeed;
     }
 
-    public void AddToScore() 
+    private void OnLevelWasLoaded(int level)
+    {
+        ResetGameSpeed();
+    }
+
+    public void AddToScore()
     {
         currentScore += pointsPerBlock;
         scoreText.text = currentScore.ToString();
+
+        AdjustGameSpeed();
+    }
+
+    private void AdjustGameSpeed()
+    {
+        if (currentScore % pointsToRaiseSpeed == 0)
+        {
+            gameSpeed += speedRaise;
+        }
+    }
+
+    private void ResetGameSpeed()
+    {
+        gameSpeed = initialGameSpeed;
     }
 
     public void Reset()
