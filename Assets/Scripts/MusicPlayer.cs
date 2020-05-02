6e8cb95429d793a135092fa17ec623d7a00f631b
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-05-02 19:05:27 +0300

    Add background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        int musicPlayersCount = FindObjectsOfType<MusicPlayer>().Length;
+        if (musicPlayersCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
